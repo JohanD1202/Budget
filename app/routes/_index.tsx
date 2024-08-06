4a5ff4f316c001c9c76202c2c2837ae9a0a934e0
Daniel_Sanabria06 <sanabria3210@gmail.com>
2024-08-06 16:04:26 -0500

    prueba

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -19,7 +19,7 @@ export default function Index() {
             href="https://remix.run/start/quickstart"
             rel="noreferrer"
           >
-            5m Quick Start
+            Pruebaaaaa
           </a>
         </li>
         <li>
